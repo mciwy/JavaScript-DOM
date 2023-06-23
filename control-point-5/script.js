97f88c66214ecec753ad23154a7b2ce159a667bc
Nikolay Sokolov <117732387+dlrxgit@users.noreply.github.com>
2023-06-23 17:33:13 +0300

    Update script.js

--- control-point-5/script.js
+++ control-point-5/script.js
@@ -1,40 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cardForm = document.getElementById('card-form');
-    const cardThumbnail = document.getElementById('card-thumbnail');
-    const cardTableBody = document.querySelector('#card-table tbody');
-  
-    cardForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      const bankName = document.getElementById('bank-name').value;
-      const paymentSystem = document.getElementById('payment-system').value;
-      const cardNumber = document.getElementById('card-number').value;
-      const cardHolder = document.getElementById('card-holder').value;
-      const expiryDate = document.getElementById('expiry-date').value;
-  
-      const newRow = document.createElement('tr');
-      newRow.innerHTML = `
-        <td>${bankName}</td>
-        <td>${paymentSystem}</td>
-        <td>${cardNumber}</td>
-        <td>${cardHolder}</td>
-        <td>${expiryDate}</td>
-      `;
-      cardTableBody.appendChild(newRow);
-  
-      cardForm.reset();
-      cardThumbnail.innerHTML = '';
-  
-      return false;
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.getElementById('cardForm');
+    var table = document.getElementById('cardTable');
+    var cardLogo = document.querySelector('.card-logo');
+    var cardDetails = document.querySelector('.card-details');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        var bankName = document.getElementById('bankName').value;
+        var paymentSystem = document.getElementById('paymentSystem').value;
+        var cardNumber = document.getElementById('cardNumber').value;
+        var cardHolder = document.getElementById('cardHolder').value;
+        var expiryDate = document.getElementById('expiryDate').value;
+
+        var newRow = table.insertRow();
+        newRow.innerHTML = '<td>' + bankName + '</td>' +
+            '<td>' + paymentSystem + '</td>' +
+            '<td>' + cardNumber + '</td>' +
+            '<td>' + cardHolder + '</td>' +
+            '<td>' + expiryDate + '</td>';
+
+        form.reset();
+        cardLogo.innerHTML = '';
+        cardDetails.innerHTML = '';
     });
-  
-    cardForm.addEventListener('input', function() {
-      const bankName = document.getElementById('bank-name').value;
-      const paymentSystem = document.getElementById('payment-system').value;
-  
-      cardThumbnail.innerHTML = `
-        <img src="logo_${bankName.toLowerCase()}_${paymentSystem.toLowerCase()}.png" alt="Card Thumbnail">
-      `;
+
+    form.addEventListener('input', function () {
+        var bankName = document.getElementById('bankName').value;
+        var paymentSystem = document.getElementById('paymentSystem').value;
+
+        cardLogo.innerHTML = bankName ? '<img src="images/' + bankName.toLowerCase() + '.png" alt="' + bankName + ' Logo">' : '';
+        cardDetails.innerHTML = paymentSystem ? '<p>' + paymentSystem + '</p>' : '';
     });
-  });
-  
+});
